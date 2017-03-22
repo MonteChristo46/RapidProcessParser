8035ca70070b5d795dc2c58ba84d4990021354cb
MonteChristo46 <dhoeschele@live.com>
2017-03-22 12:50:16 +0100

    Class OutParser ist able to catch all (!) data from the database.

--- php/OutParser.php
+++ php/OutParser.php
@@ -6,7 +6,9 @@
  * Time: 19:27
  */
 require_once("Parser.php");
-
+require_once("ProcessInstance.php");
+require_once("Activity.php");
+require_once("Attribute.php");
 class outParser extends Parser
 {
     private $db;
@@ -28,25 +30,26 @@ class outParser extends Parser
         $resultForProcessInstances = mysqli_query($this->db, $sqlForProcessInstances);
         while($pi =  mysqli_fetch_array($resultForProcessInstances, MYSQLI_ASSOC)){
             $processInstance = new ProcessInstance($pi["Name"]);
-            //$processInstance->setId($pi["P_ID"]);
+            $processInstance->setId($pi["P_ID"]);
             $processInstance->setDate($pi["Date"]);
             $processInstanceID = $pi["P_ID"];
-            //$this->processInstances[] = $processInstance; --> Erst am Ende
             //Get Corresponding Activities for ProcessInstance
             $sqlForActivities = "SELECT `Activity_name`, `A_ID` FROM `Activity` WHERE `P_ID` = '$processInstanceID'";
             $resultForActivities = mysqli_query($this->db, $sqlForActivities);
-            while($ac = mysqli_fetch_array($resultForProcessInstances, MYSQLI_ASSOC)){
+            while($ac = mysqli_fetch_array($resultForActivities, MYSQLI_ASSOC)){
                 $activity = new Activity($ac["Activity_name"]);
                 $activityID = $ac["A_ID"];
                 //Get Corresponding Attributes to Activity
                 $sqlForAttributes = "SELECT `Attr_Name`, `Attr_Value` FROM `Attribute` WHERE `A_ID` = '$activityID'";
                 $resultForAttributes = mysqli_query($this->db, $sqlForAttributes);
                 while($at =  mysqli_fetch_array($resultForAttributes, MYSQLI_ASSOC)){
-                    $activity->addAttribute($at["Attr_Name"], $ac["Attr_Value"]);
+                    $activity->addAttribute($at["Attr_Name"], $at["Attr_Value"]);
                 }
+                $processInstance->addActivity($activity);
             }
+            $this->processInstances[] = $processInstance;
         }
-
+        return $this->processInstances;
     }
 
     public function parseDataToXES(){
