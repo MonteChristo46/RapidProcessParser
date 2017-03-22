21a012cc81be747df700f5f6c02be2205fa68548
richardhorn <richard.horn94@yahoo.de>
2017-03-22 15:45:49 +0100

    Update Parser:
    --> InParser working stable version 1
    --> Upload in Interface starts Parser directly and uploads file to /upload

--- index.php
+++ index.php
@@ -50,19 +50,12 @@
             if(count($_FILES['upload']['name']) > 0){
                 //Loop through each file
                 for($i=0; $i<count($_FILES['upload']['name']); $i++) {
-                    //Get the temp file path
                     $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
-
-                    //Make sure we have a filepath
                     if($tmpFilePath != ""){
 
-                        //save the filename
                         $shortname = $_FILES['upload']['name'][$i];
-
-                        //save the url and the file
                         $filePath = "upload/" .$_FILES['upload']['name'][$i];
 
-                        //Upload the file into the temp dir
                         if(move_uploaded_file($tmpFilePath, $filePath)) {
 
                             $files[] = $shortname;
