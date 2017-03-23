b1bec844a08169e2f69c1279c06fa2d5978be3cd
richardhorn <richard.horn94@yahoo.de>
2017-03-23 16:56:49 +0100

    Update Parser:
    --> Small interface changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,51 @@
-/**
- * Created by danielhoschele on 19.03.17.
- */
+function toDo(obj){
+    expandDiv(obj);
+    countNumbers(obj);
+}
+
+function expandDiv(object){
+    $(object).find(".box").slideToggle('slow', function(){
+        var bodyHeight = $('body').height();
+        var rightContent = $(object);
+        var heading = rightContent.find(".rightContentHeading");
+        var rightContentTopHeight = Math.round((rightContent.height()/bodyHeight)*100);
+        var newHeight = 0.6; // in percentage
+        if(rightContentTopHeight/100 < newHeight){
+            //heading.css("display", "none");
+            rightContent.height(newHeight*bodyHeight);
+            rightContent.siblings().height(Math.round(((1-newHeight)/2)*100)+"%");
+            rightContent.siblings().attr("onclick","");
+        }else if(rightContentTopHeight/100 == newHeight){
+            //heading.css("display", "block");
+            rightContent.height(33.3+"%"); // Height of rightContentTop
+            rightContent.siblings().height(33.3+"%");
+            rightContent.siblings().attr("onclick","expandDiv(this)");
+        }
+    });
+
+}
+
+function countNumbers(object) {
+    var opened = false;
+    var boxHeight = $(object).height();
+    var bodyHeight = $('body').height();
+    var actualHeight = Math.round((boxHeight/bodyHeight)*100);
+    console.log(actualHeight);
+    var startingHeight = 33;
+    if(actualHeight <= startingHeight){
+        var opened = true;
+    }
+    if(opened) {
+        $('.automaticNumberCounter').each(function () {
+            var $this = $(this);
+            //Value anstelle von text(), da der Counter den Text verändert
+            jQuery({Counter: 0}).animate({Counter: $this.attr('value')}, {
+                duration: 2000,
+                easing: 'swing',
+                step: function (now) {
+                    $this.text(Math.ceil(now));
+                }
+            });
+        });
+    }
+}
---
+++ php/interfaceLogic.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: richardhorn
+ * Date: 23.03.17
+ * Time: 14:43
+ */
+
+function alert($message){
+    echo '<script type="text/javascript" language="Javascript">';
+    echo 'alert("'.$message.'");';
+    echo '</script>';
+}
+require_once ("php/DataBaseInterface.php");
+require_once ("php/InParser.php");
+
+//Get relevant numbers for interface
+$dbi = new DataBaseInterface();
+$instances = $dbi->getLastInstanceID();
+$activities = $dbi->getLastActivityID();
+$attributes = $dbi->getLastAttributeID();
+$oldest = $dbi->getDateOfFirstInstance();
+$newest = $dbi->getDateOfLastInstance();
+
+//Upload the given files
+if(isset($_POST['submit'])){
+    $count = 0;
+    $uploadedFiles = count($_FILES['upload']['name']);
+
+    if($uploadedFiles > 0){
+        for($i=0; $i<count($_FILES['upload']['name']); $i++) {
+
+            if ($_FILES['upload']['extension'] != "xml") {
+                echo "Error: Please upload XML Files";
+            } else {
+                $tmpFilePath = $_FILES['upload']['tmp_name'][$i];
+                if ($tmpFilePath != "") {
+
+                    $shortname = $_FILES['upload']['name'][$i];
+                    $filePath = "upload/" . $_FILES['upload']['name'][$i];
+
+                    if (move_uploaded_file($tmpFilePath, $filePath)) {
+
+                        $files[] = $shortname;
+                        $count += 1;
+                        $parser = new inParser($filePath);
+                        $parser->parseInDatabase();//Anpassen wenn Abstrakte Klasse fertig!
+                    }
+                }
+            }
+        }
+    }
+    if($count == $uploadedFiles){
+        alert("Success: Uploaded and stored ".$count." File(s) in Database!");
+    }
+}
+
+
